b7b1d2cc335967d8cf4312f96a259820caae5748
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2023-01-26 18:15:15 +0500

    changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Header from "./Components/Header";
-import Form from "./Components/Form";
 import { Card } from "./Components/Card";
 import "./Components/Header.scss";
 import "./Components/Country.scss";
@@ -8,6 +7,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useRef } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Loading from "./assets/images/Spinner.svg"
 
 function App() {
   const elInput = useRef();
@@ -48,7 +48,7 @@ function App() {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    if (elInput.current.value != "") {
+    if (elInput.current.value !== "") {
       fetch("https://restcountries.com/v3.1/name/" + elInput.current.value)
         .then((res) => res.json())
         .then((data) => {
@@ -138,7 +138,16 @@ function App() {
         </form>
       </section>
       <div className=" container">
-        {country.isLoading ? <h1>Loading...</h1> : ""}
+        {country.isLoading ? <h1><img style={{
+          position:"absolute",
+          top:"0px",
+          bottom:"0px",
+          right:"0px",
+          left:"0px",
+          margin:"0 auto",
+          width:"200px",
+          height:"200px"
+        }} src={Loading} alt="load"/></h1> : ""}
         {country.isError ? <h1>{country.isError}</h1> : ""}
         {country.data.length ? (
           <ul className="row  gap-5 justify-content-center mt-4 list-unstyled ">
